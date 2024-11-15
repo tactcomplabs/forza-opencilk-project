3ebc8e029b488c05112ca6e2f002b4a87600f11f
John Leidel <john.leidel@gmail.com>
2024-11-15 12:44:06 -0600

    updating MCOPY instruction encoding; adding RMAC integer instructions

--- llvm/test/MC/RISCV/forza-thread-valid.s
+++ llvm/test/MC/RISCV/forza-thread-valid.s
@@ -17,17 +17,32 @@ quit zero, zero, 0
 # CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
 quit_thread
 
-# CHECK-ASM-AND-OBJ: sendack a5, a2, 0
-# CHECK-ASM: encoding: [0xab,0x47,0x06,0x00]
+# CHECK-ASM-AND-OBJ: sendack a5, a2, a2
+# CHECK-ASM: encoding: [0x8b,0x07,0xc6,0xa0]
 # CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
-sendack a5, a2, 0
+sendack a5, a2, a2
 
 # CHECK-ASM-AND-OBJ: spawn a4, ra, s0
 # CHECK-ASM: encoding: [0x77,0x87,0x80,0x00]
 # CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
 spawn a4, ra, s0
 
-# CHECK-ASM-AND-OBJ: mcopy a0, 64(a2)
-# CHECK-ASM: encoding: [0x23,0x40,0xa6,0x04]
+# CHECK-ASM-AND-OBJ: mcopy a0, a1, a2
+# CHECK-ASM: encoding: [0x6b,0x00,0xb6,0x50]
 # CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
-mcopy a0, 64(a2)
+mcopy a0, a1, a2
+
+# CHECK-ASM-AND-OBJ: rmac.ww a0, a1, a3
+# CHECK-ASM: encoding: [0x7f,0x80,0xd5,0x50]
+# CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
+rmac.ww a0, a1, a3
+
+# CHECK-ASM-AND-OBJ: rmac.wd a0, a1, a3
+# CHECK-ASM: encoding: [0x7f,0x90,0xd5,0x50]
+# CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
+rmac.wd a0, a1, a3
+
+# CHECK-ASM-AND-OBJ: rmac.dd a0, a1, a3
+# CHECK-ASM: encoding: [0x7f,0xa0,0xd5,0x50]
+# CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
+rmac.dd a0, a1, a3
