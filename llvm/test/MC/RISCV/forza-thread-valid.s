b72c0366a865c77422246fd73befe63a617ecf00
John Leidel <john.leidel@gmail.com>
2024-11-25 10:23:30 -0600

    adding pzop.ii instruction

--- llvm/test/MC/RISCV/forza-thread-valid.s
+++ llvm/test/MC/RISCV/forza-thread-valid.s
@@ -46,3 +46,8 @@ rmac.wd a0, a1, a3
 # CHECK-ASM: encoding: [0x3f,0xa0,0xd5,0x50]
 # CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
 rmac.dd a0, a1, a3
+
+# CHECK-ASM-AND-OBJ: pzop.ii a0, a1, a2, 1
+# CHECK-ASM: encoding: [0xeb,0x10,0xb5,0x60]
+# CHECK-RV32: :[[@LINE+1]]:1: error: instruction requires the following: RV64I Base Instruction Set
+pzop.ii a0, a1, a2, 1
