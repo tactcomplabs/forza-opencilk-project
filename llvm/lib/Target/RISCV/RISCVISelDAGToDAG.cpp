dd9f7a8d4f04cfa6e86fa703c26bdd2a75cf04fc
rkabrick <rkabrick@udel.edu>
2024-12-03 10:48:36 -0500

    Adding single 32 bit intrinsic to ISelDAGToDAG for showing the impossible reg2reg copy error

--- llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
+++ llvm/lib/Target/RISCV/RISCVISelDAGToDAG.cpp
@@ -230,6 +230,8 @@ unsigned RISCVDAGToDAGISel::getOpcodeForIntrinsic(unsigned IntNo) {
       return RISCV::AMO_R_XOR64REMNO;
     case Intrinsic::riscv_forza_amo_r_xor64rem_on:             // llvm.riscv.forza.amo.r.xor64rem.on
       return RISCV::AMO_R_XOR64REMON;
+    case Intrinsic::riscv_forza_amo_r_fadd32u:             // llvm.riscv.forza.amo.r.xor64rem.on
+      return RISCV::AMO_R_FADD32U;
   default:
     llvm_unreachable("Unknown intrinsic");
   }
